c024acb2e0929af71592a0792e7e7ab6a320e4a8
erfan kheyrollahi <ekm507@gmail.com>
2019-08-08 23:57:19 +0430

    code added

---
+++ common.py
@@ -0,0 +1,34 @@
+import io
+
+textBook = io.open('snowcrash.txt', 'r', encoding='utf-8')
+
+wordList = list()
+for line in textBook:
+    wordList.extend(list(str(line).split(' ')))
+
+wordDict = dict()
+
+for word in wordList:
+    wordDict[word] = 0
+
+for word in wordList:
+    wordDict[word] += 1
+
+sortedWordDict = list(sorted(wordDict.items(), key=lambda x: x[1], reverse=True))
+
+print(len(wordDict), ' words found.')
+
+print('most common words:')
+
+outputFile = io.open('common.txt', 'w', encoding='utf-8')
+
+i = 0
+for k in sortedWordDict:
+    i += 1
+    outputFile.write(str(k[0]) + ' ' + str(k[1]) + '\n')
+    if(i < 20):
+        print(i, k)
+
+
+textBook.close()
+outputFile.close()
